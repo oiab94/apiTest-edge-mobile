c49858a35b1668eda1705d9aecc33b19bc248dea
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-05-30 17:48:39 -0400

    [test] Prueba de /api/people/:id

--- test/people.test.js
+++ test/people.test.js
@@ -19,4 +19,23 @@ describe("Prueba sobre la API", () => {
 			expect(response.body).toBeInstanceOf(Object);
 		})
 	});
+
+
+	describe("GET /api/people/:id", () => { 
+		let response;
+
+		it("Encuentra el id retorna status 200 y el objeto", async () => {
+			const id = 12;
+			response = await request(app).get(`/api/people/${id}`);
+
+			expect(response.status).toBe(200);
+		})
+
+		it("No encuentra el id retorna status 400 y un objeto", async () => {
+			const id = 122;
+			response = await request(app).get(`/api/people/${id}`);
+
+			expect(response.status).toBe(400);
+		})
+	})
 });
