f7c31196d27d94532f447e28c380fc1e56ff781b
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-05-30 14:53:50 -0400

    Controlador que busca los datos de una persona a tráves de su nombre

--- src/controllers/people.controller.js
+++ src/controllers/people.controller.js
@@ -6,7 +6,7 @@ const { handleErrors } = require("../components/handleErrors");
  * * Permite obtener los datos de todos los personajes de Star Wars
  */
 const getAllPeople = (req, res) => {
-	axios.get(process.env.API + "/api/peopale")
+	axios.get(process.env.API + "/api/people")
 	// Busca y envia todos los datos de la API SWAPI
 		.then(({ data }) => res.status(200).json(data))
 
@@ -38,7 +38,32 @@ const getPeopleById = (req, res) => {
 		});
 };
 
+/**
+ * * Permite buscar los datos de un personaje
+ */
+const getCharacterBySearch = (req, res) => {
+	const { search } = req.query;
+
+	axios.get(process.env.API + "/api/people/?search=" + search)
+		// Busca y envia los datos a tráves del query search
+		.then(({ data }) => {
+			let { results } = data;
+
+			if(Array.isArray(results))
+				res.status(200).json(results);
+			else 
+				// Si SWAPI no retorna un array, entonces indicamos que hubo en error en el server
+				res.status(500).json({"message": "An internal server error has occured"});
+		})
+		.catch(err => {
+			let error = handleErrors(err);
+
+			res.status(400).json(error);
+		});
+};
+
 module.exports = {
 	getAllPeople,
-	getPeopleById
+	getPeopleById,
+	getCharacterBySearch
 };
