6eae78f7638b21e15973dd244589b2046010b4b6
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-05-30 14:08:57 -0400

    Controlador que busca una persona a tráves de un ID en la api SWAPI

--- src/controllers/people.controller.js
+++ src/controllers/people.controller.js
@@ -1,12 +1,12 @@
+const axios = require("axios").default;
 const { handleErrors } = require("../components/handleErrors");
 
-const axios = require("axios").default;
 
 /**
  * * Permite obtener los datos de todos los personajes de Star Wars
  */
 const getAllPeople = (req, res) => {
-	axios.get(process.env.API + "/api/people")
+	axios.get(process.env.API + "/api/peopale")
 	// Busca y envia todos los datos de la API SWAPI
 		.then(({ data }) => res.status(200).json(data))
 
@@ -18,6 +18,27 @@ const getAllPeople = (req, res) => {
 		});
 };
 
+/**
+ * * Permite obtener los datos de un personaje en especifico a tráves de un id 
+ */
+const getPeopleById = (req, res) => {
+	const { id } = req.params;
+
+	axios.get(process.env.API + "/api/people/" + id)
+		// Busca y envia los datos por medio de un ID
+		.then(({ data }) => res.status(200).json(data))
+		
+		// Se controla y envia los errores recibidos
+		.catch(err => {
+			let error = handleErrors(err);
+
+			error = { ...error, id};
+
+			res.status(400).json(error);
+		});
+};
+
 module.exports = {
 	getAllPeople,
+	getPeopleById
 };
