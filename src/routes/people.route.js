9808e3adab3f8a43f92ba7338e91726579a69788
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-05-30 14:10:00 -0400

    Ruta de conexion a getPeopleById de nuestra API

--- src/routes/people.route.js
+++ src/routes/people.route.js
@@ -1,10 +1,13 @@
 const { Router } = require("express");
-const { getAllPeople } = require("../controllers/people.controller");
+const { 
+	getAllPeople, 
+	getPeopleById } = require("../controllers/people.controller");
 
 // * Variables
 const router = Router();
 
 // * Direccionamiento
 router.get("/api/people", getAllPeople);
+router.get("/api/people/:id", getPeopleById);
 
 module.exports = router;
