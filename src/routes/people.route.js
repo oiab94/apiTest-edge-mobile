255caa059770b3d45722e85ce06398b635e4bd59
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-05-31 01:07:18 -0400

    Indica como hacer uso de la ruta /api/people

--- src/routes/people.route.js
+++ src/routes/people.route.js
@@ -8,6 +8,19 @@ const {
 const router = Router();
 
 // * Direccionamiento
+
+/**
+ * @swagger
+ * /api/people:
+ *  get:
+ *     summary: Return a object with all the characters from Star Wars
+ *     description: Get all the Star Wars characters from SWAPI
+ *     responses:
+ *      200:
+ *         description: OK
+ *      400:
+ *         description: Bad Request
+ */
 router.get("/api/people", getAllPeople);
 router.get("/api/people/:id", getPeopleById);
 router.get("/api/peopleSearch/", getCharacterBySearch);
